315b4ce044b74cad27fbfe646607e78ef2da22b8
phoenix021 <jelenaziv007@gmail.com>
2023-10-05 12:22:44 +0200

    JPA and liquibase integration
    + Configured jpa and integrated liquibase to use default datasource
    + Enabled console access for hibernate

--- src/test/java/com/accelex/sample/exercise/ExerciseApplicationTests.java
+++ src/test/java/com/accelex/sample/exercise/ExerciseApplicationTests.java
@@ -1,13 +1,38 @@
 package com.accelex.sample.exercise;
 
+import java.util.List;
+
+import com.accelex.sample.exercise.entity.Customer;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class ExerciseApplicationTests {
+	
+    private final TestRestTemplate restTemplate = new TestRestTemplate();
 
 	@Test
 	void contextLoads() {
 	}
+	
+    @Test
+    public void getAllCustomersEndpoint() {
+        String url = "http://localhost:8080/api/customer/getAll";
+
+        // Make a GET request
+        //ResponseEntity<List> response = restTemplate.getForEntity(url, List.class);
+
+        // Assert the response
+       // Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
+    }
 
 }
+
+
+
